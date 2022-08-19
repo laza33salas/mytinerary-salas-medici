e64403c39ace52ae68f6f2c72bb8c9d0cbd925a5
LeonardoMedici <leomedici06@gmail.com>
2022-08-19 00:25:57 -0300

    Agregamos media query

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
 return(
     <div className="hero-container">
         <h1 className="hero-tittle">MyTinerary</h1>
-        <p className="hero-slogan">Find your perfect trip, designed by insiders who know and love their citties!</p>
+        <h3 className="hero-slogan">Find your perfect trip, designed by insiders who know and love their citties!</h3>
         <button className="hero-button">Pagina principal</button>
     </div>
     
