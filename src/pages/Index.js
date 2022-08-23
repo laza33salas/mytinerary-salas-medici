656df87349dc47f1b2f3ed5956a468237a3bbf5d
laza33salas <lazarocode@gmail.com>
2022-08-23 15:35:11 -0300

    add cityPage and NewCity

---
+++ src/pages/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../Layouts/Layout"
+import Hero from "../components/Hero/Hero"
+export const Index = () => {
+  return (
+    <>
+    <Hero/>
+    <Layout/>
+    </>
+  )
+}
+
+export default Index
