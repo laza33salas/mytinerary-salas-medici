9af90e6326224fd9f9a6517a89d74c1c89eaa851
laza33salas <lazarocode@gmail.com>
2022-08-23 15:36:43 -0300

    creacion de call to action ;)

---
+++ src/components/Hero/CallToAction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./CallToAction.css"
+
+export default function CallToAction() {
+  return (
+    <div>
+    <a href="#" class="cta">
+     <span>GET STARTED</span>
+    <svg viewBox="0 0 13 10" height="10px" width="15px">
+    <path d="M1,5 L11,5"></path>
+    <polyline points="8 1 12 5 8 9"></polyline>
+    </svg>
+    </a>
+    </div>
+  )
+}
+
