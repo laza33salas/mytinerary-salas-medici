318a6963654c41dc6bcb4cb72760277c6ed8aaa2
laza33salas <lazarocode@gmail.com>
2022-08-24 18:26:52 -0300

    add burgerButton

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,7 +1,9 @@
 
+import React, { useState } from 'react'
 import {Link as LinkRouter } from 'react-router-dom'
 import "./Nav.css"
-
+  
+const Nav = () => {
 const pages = [
   {name: "Home", to:"/", imagen:"https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt:"casa"},
   {name: "Cities", to:"/cities", imagen:"https://cdn-icons-png.flaticon.com/512/2222/2222208.png",alt:"cities"},
@@ -10,16 +12,32 @@ const pages = [
 
 const link = (page) =>
 <LinkRouter className="containerLinks" to={page.to}>
-  { <img alt={page.alt}  className="logos" src={page.imagen}/>}
- <p className='parrafos'>{page.name}</p>  
+  
+    {<img alt={page.alt}  className="logos" src={page.imagen}/>}
+    <p className='parrafos'>{page.name}</p>  
+  
  </LinkRouter>
 
+ // BURGER BUTTON 
+  const [open, setOpen] = useState(false)
+  const handleOpenMenu = () =>{
+      if (open) {
+        setOpen(false)
+      }else{
+        setOpen(true)
+      }
+  }
 
-const Nav = () => {
- 
   return (
     <div className='nav'>
-        {pages.map(link)}
+      <div>
+        {open
+          ? pages.map(link)
+          : null
+        }
+      </div>
+      {pages.map(link)}
+      <button onClick={handleOpenMenu}>Boton</button>
     </div>
   )
 }
