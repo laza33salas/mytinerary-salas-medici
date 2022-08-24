d7f2fc89c5612ac4968adb60276e1e551ff97d71
laza33salas <lazarocode@gmail.com>
2022-08-24 10:28:16 -0300

    add nav dinamico y correccion callToAction

--- src/components/Hero/CallToAction.js
+++ src/components/Hero/CallToAction.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import {Link as LinkRouter} from "react-router-dom"
 import "./CallToAction.css"
 
 export default function CallToAction() {
+  let linkTo = "/cities"
+  
   return (
     <div>
-    <a href="#" class="cta">
+    <LinkRouter to={linkTo} className="cta">
      <span>GET STARTED</span>
     <svg viewBox="0 0 13 10" height="10px" width="15px">
     <path d="M1,5 L11,5"></path>
     <polyline points="8 1 12 5 8 9"></polyline>
     </svg>
-    </a>
+    </LinkRouter>
     </div>
   )
 }
