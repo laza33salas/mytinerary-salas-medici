e5ca0d9bb607a47ab43290ba84a169e325dba4f3
laza33salas <lazarocode@gmail.com>
2022-08-26 16:20:22 -0300

    agregamos newCity a footerNav

--- src/components/Footer/FooterNav.js
+++ src/components/Footer/FooterNav.js
@@ -8,7 +8,8 @@ const FooterNav = () => {
   
 const navegacion = [
   {name: "Home", to:"/"},
-  {name: "Cities", to:"/cities"}
+  {name: "Cities", to:"/cities"},
+  {name: "New City", to:"/newcity"}
 ]
 
 const linkNav = (item) => 
