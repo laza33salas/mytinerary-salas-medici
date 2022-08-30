d99d892aa5b20c23ccd9222f8c1757c0ec457390
laza33salas <lazarocode@gmail.com>
2022-08-30 20:06:37 -0300

    create folder input

---
+++ src/components/Input/Input.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import "./Input.css"
+
+
+const Input = (props) => {
+//Setear los hooks useState
+
+const [search, setSearch] = useState("")  
+
+//Capturar input
+const searcher = e =>{
+  setSearch(e.target.value)
+  console.log(e.target.value)
+}
+
+  return (
+    <div className='form-container'>
+      <form >
+        <legend>{props.dato}</legend>
+        <input className='input-style' type="text" placeholder={props.placeholder} value={search} onChange={searcher}/>    
+      </form>
+    
+    </div>
+  )
+}
+
+export default Input
