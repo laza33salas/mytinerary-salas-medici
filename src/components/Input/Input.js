3dcae9ee2161f7204b23fbcf53f34b4161d1a448
laza33salas <lazarocode@gmail.com>
2022-09-01 02:07:41 -0300

    use useRef

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,23 +1,24 @@
-import {useState} from 'react'
+import {useState,useRef, useEffect} from 'react'
 import "./Input.css"
 
 
 const Input = (props) => {
 //Setear los hooks useState
 
-const [search, setSearch] = useState("")  
+const [inputValue, setinputValue] = useState("")  
+const previousinputValue = useRef("")
 
 //Capturar input
-const searcher = e =>{
-  setSearch(e.target.value)
-  console.log(e.target.value)
-}
+useEffect(() => {
+previousinputValue.current = inputValue;
+}, [inputValue])
 
   return (
     <div className='form-container'>
       <form >
         <legend className="legendInput">{props.dato}</legend>
-        <input className='input-style' type="text" placeholder={props.placeholder} value={search} onChange={searcher}/>    
+        <input className='input-style' type="text" placeholder={props.placeholder} value={inputValue} onChange={(e) => setinputValue(e.target.value)}/>    
+      {console.log(inputValue)}
       </form>
     
     </div>
