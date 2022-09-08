6841df3dc67a64926c42d9ef542c213f6721634d
LeonardoMedici <leomedici06@gmail.com>
2022-09-08 01:47:13 -0300

    Carousel with REDUX

--- src/components/CarouselCity/CarouselCity.js
+++ src/components/CarouselCity/CarouselCity.js
@@ -3,23 +3,38 @@ import "./CarouselCity.css"
 import PopularCities from "./PopularCities"
 import axios from "axios"
 import {useEffect, useState} from "react"
-
+import { useGetAllCitiesQuery } from "../../features/citiesApi"
 
 
 function CarouselCity() {
-    const [cities, setCities] = useState([])
+   const [value, setValue] = useState([])
+   const {
+    //informacion que necesito usar, es el body
+    data: cities ,
+    //
+    error,
+    //Una propiedad que me indica si se esta cargando ese dato
+    isLoading,
+    //Una propiedad que me indica si se completo la carga con exito
+    isSuccess,
+    //Una propiedad en caso de que haya fallado
+    isFailed
     
-    useEffect(()=>{
+  } = useGetAllCitiesQuery(value)
+    
+    
+   /* useEffect(()=>{
         axios.get("http://localhost:4000/cities/")
           .then(response => setCities(response.data.response))
           
-      },[])
+      },[]) */
+    
+  const citiesCarousel = cities.response.slice(0, 12)
 
-      const slice = cities.slice(0,12)
     return (
         <div className="container-carousel-city">
-            <PopularCities/>
-            <Carousel data={slice} range={4}/>
+           <PopularCities/>
+           <Carousel data={citiesCarousel} range={4}/>
         </div>
     )
 }
