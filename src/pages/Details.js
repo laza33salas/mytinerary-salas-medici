510c0fc1412d07fa7689902180a7f43cef1a3954
laza33salas <lazarocode@gmail.com>
2022-09-07 22:45:41 -0300

    add component itenirary to CardsDetails

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import CardDetails from '../components/CardDetails/CardDetails'
-
+import Itinerary from '../components/Itinerary/Itinerary'
 const Details = () => {
   
   
   return (
     <>
       <CardDetails/>
+      <Itinerary/>
     </>
   )
 }
