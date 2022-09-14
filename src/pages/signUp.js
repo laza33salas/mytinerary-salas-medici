a69a81d3ec22c2744736a38a51509bfaa9900fb7
LeonardoMedici <leomedici06@gmail.com>
2022-09-14 13:13:32 -0300

    Cambio de estilos para página sign up

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import SignUpGoogle from '../components/SignUpGoogle/SignUpGoogle'
+import "./SignUp.css"
 
 export default function SignUp() {
   return (
-    <div>
-        <SignUpGoogle/>
+    <div className='sign-up-page'>
+        <p className='p-detail'>Sign Up with:</p>
         <form action="">
             <input type="text" />
         </form>
+        <p className='p-detail'>Or sign up with:</p>
+        <SignUpGoogle/>
     </div>
   )
 }
