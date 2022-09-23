e77edefe694bcb82eec109fdf0d86b90204dcbef
laza33salas <lazarocode@gmail.com>
2022-09-23 11:03:29 -0300

    use state and use effect welldone

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react'
-import { useRef } from 'react'
+
 import { Link as LinkRouter } from 'react-router-dom'
 import { useSelector } from "react-redux";
 import "./Nav.css"
@@ -10,29 +10,32 @@ const Nav = () => {
   const rolUsuario = useSelector(state => state.user.user.role)//Agarra desde el storage el rol.
   let[pages, setPages] = useState([])
 
-  if (rolUsuario == 'admin') {
-    pages = [
-      { name: "Home", to: "/", imagen: "https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt: "casa" },
-      { name: "Cities", to: "/cities", imagen: "https://cdn-icons-png.flaticon.com/512/2222/2222208.png", alt: "cities" },
-      { name: "New City", to: "/newcity", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "new-city" },
-      { name: "Edit City", to: "/edit-city", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "edit-city" },
-      { name: "MyTinerary", to: "/myTineraries", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "MyTinerary" }
-    ]
-  } else if (rolUsuario == 'user') {
-    pages = [
-      { name: "Home", to: "/", imagen: "https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt: "casa" },
-      { name: "Cities", to: "/cities", imagen: "https://cdn-icons-png.flaticon.com/512/2222/2222208.png", alt: "cities" },
-      { name: "MyTinerary", to: "/myTineraries", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "MyTinerary" }
-    ]
-  } else {
-    pages = [
-      { name: "Home", to: "/", imagen: "https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt: "casa" },
-      { name: "Cities", to: "/cities", imagen: "https://cdn-icons-png.flaticon.com/512/2222/2222208.png", alt: "cities" },
-    ]
-  }
+  useEffect(()=>{
+    if (rolUsuario === 'admin') {
+      setPages([
+        { name: "Home", to: "/", imagen: "https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt: "casa" },
+        { name: "Cities", to: "/cities", imagen: "https://cdn-icons-png.flaticon.com/512/2222/2222208.png", alt: "cities" },
+        { name: "New City", to: "/newcity", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "new-city" },
+        { name: "Edit City", to: "/edit-city", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "edit-city" },
+        { name: "MyTinerary", to: "/myTineraries", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "MyTinerary" }
+      ])
+    } else if (rolUsuario === 'user') {
+      setPages([
+        { name: "Home", to: "/", imagen: "https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt: "casa" },
+        { name: "Cities", to: "/cities", imagen: "https://cdn-icons-png.flaticon.com/512/2222/2222208.png", alt: "cities" },
+        { name: "MyTinerary", to: "/myTineraries", imagen: "https://cdn-icons-png.flaticon.com/512/2451/2451548.png", alt: "MyTinerary" }
+      ])
+    } else {
+      setPages([
+        { name: "Home", to: "/", imagen: "https://cdn-icons-png.flaticon.com/512/2413/2413074.png", alt: "casa" },
+        { name: "Cities", to: "/cities", imagen: "https://cdn-icons-png.flaticon.com/512/2222/2222208.png", alt: "cities" },
+      ])
+    }
+  },[rolUsuario])
+  
 
   const link = (page) =>
-    <LinkRouter className="containerLinks" to={page.to}>
+    <LinkRouter className="containerLinks" to={page.to} key={page.alt}>
       {<img alt={page.alt} className={"logos " + page.alt} src={page.imagen} />}
       <p className='parrafos'>{page.name}</p>
     </LinkRouter>
@@ -47,9 +50,7 @@ const Nav = () => {
     }
   }
 
-  useEffect(() => {
-    setPages(pages)
-  },[pages])
+
 
   return (
     <div className='nav'>
