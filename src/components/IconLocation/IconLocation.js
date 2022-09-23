5718daa118dfa6c79a11b973153f150046606018
laza33salas <lazarocode@gmail.com>
2022-09-22 22:00:23 -0300

    create components leaflet

---
+++ src/components/IconLocation/IconLocation.js
@@ -0,0 +1,13 @@
+
+import L from 'leaflet'
+
+export const IconLocation = L.icon({
+    iconUrl: require('../../Assets/icono-ubicacion.webp'),
+    iconRetinaUrl: require('../../Assets/icono-ubicacion.webp'), iconAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: [35, 35],
+    className: "leaflet-venue-icon",
+})  
+
